2f29c8461534053d73b42417a23d3e7478330935
Masumpa Hlongwane <sanelehlongwane@gmail.com>
2024-06-14 21:26:34 +0200

    Alot is happening

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,14 @@ import { ClerkProvider } from '@clerk/nextjs';
 import NavBar from '../components/NavBar';
 import '../globals.css';
 
-export default function MyApp({ Component, pageProps }) {
+// Define types for Component and pageProps
+type MyAppProps = {
+  Component: any; // Adjust 'any' to the specific type of your Component if known
+  pageProps: any; // Adjust 'any' to the specific type of your pageProps if known
+};
+
+// Use MyAppProps as the type for props
+export default function MyApp({ Component, pageProps }: MyAppProps) {
   return (
     <ClerkProvider>
       <NavBar />
